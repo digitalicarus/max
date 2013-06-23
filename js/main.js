4e708d85eeca1bf8b65d9f98fe198f7f905a364a
digitalicarus <digitalicarus@gmail.com>
2013-06-23 12:02:32 -0500

    backbone skeleton

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
-/*global define:true*/
-define([], function () {
+/*global define:true,require:true,Backbone:true,$:true,_:true*/
+require.config({
+	paths: {
+		'backbone': 'vendor/backbone-min',
+		'underscore': 'vendor/underscore-min',
+		'jquery': 'vendor/jquery-2.0.2.min'
+	},
+	shim: {
+		'backbone': {
+			deps: ['underscore', 'jquery'],
+			exports: function () {
+				"use strict";
+				return Backbone.noConflict();
+			}
+		},
+		'underscore': {
+			exports: function() {
+				"use strict";
+				return _.noConflict();
+			}
+		},
+		'jquery': {
+			exports: function() {
+				"use strict";
+				return $.noConflict();
+			}
+		}
+	}
+});
+
+define('main', ['backbone', 'router'], function (BB, Router) {
 	"use strict";
 });
+
+require(['main'], function (app) {
+	"use strict";
+});
+
+
